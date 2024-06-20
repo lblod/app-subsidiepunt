c38555a67c34ebcd82dc0054379d81460df3c80a
felix <ruiz.felixruiz@gmail.com>
2024-06-20 15:30:55 +0200

    remove download url

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -1,7 +1,6 @@
 import deltaProducerDumpFilePublisher from './delta-producer-dump-file-publisher';
 import deltaProducerPublicationGraphMaintainerSubsidies from './delta-producer-publication-graph-maintainer-subsidies';
 import deltaProducerReportGenerator from './delta-producer-report-generator';
-import downloadUrl from './download-url';
 import errorAlert from './error-alert';
 import jobsController from './jobs-controller';
 import resource from './resource';
@@ -10,7 +9,6 @@ export default [
   ...deltaProducerDumpFilePublisher,
   ...deltaProducerPublicationGraphMaintainerSubsidies,
   ...deltaProducerReportGenerator,
-  ...downloadUrl,
   ...errorAlert,
   ...jobsController,
   ...resource
