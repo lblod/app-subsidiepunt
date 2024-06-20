edfa9d2c102aba3e43a4709216415aceb1f4e1d3
felix <ruiz.felixruiz@gmail.com>
2024-06-20 11:36:46 +0200

    update deltanotifier rules

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -5,7 +5,6 @@ import downloadUrl from './download-url';
 import errorAlert from './error-alert';
 import jobsController from './jobs-controller';
 import resource from './resource';
-import validateSubmission from './validate-submission';
 
 export default [
   ...deltaProducerDumpFilePublisher,
@@ -14,6 +13,5 @@ export default [
   ...downloadUrl,
   ...errorAlert,
   ...jobsController,
-  ...resource,
-  ...validateSubmission,
+  ...resource
 ];
