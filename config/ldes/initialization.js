edb9eb731fa0289bf0c3739c3b8c4f7fe38f58ec
Achraf <achrafatauil@hotmail.com>
2025-04-29 17:18:44 +0200

    add initialization for subsidie status

--- config/ldes/initialization.js
+++ config/ldes/initialization.js
@@ -1,34 +1,12 @@
-const regularTypes = {
-  "http://www.w3.org/ns/adms#Status": "public",
-  "http://www.w3.org/ns/adms#Identifier": "abb",
-  "http://schema.org/ContactPoint": "abb",
-  "http://www.w3.org/ns/locn#Address": "abb",
-};
-
 export const initialization = {
   public: {
-    "http://lblod.data.gift/vocabularies/subsidie/ApplicationForm": {
+    "http://data.vlaanderen.be/ns/subsidie#SubsidiemaatregelConsumptie": {
       filter: `
-        OPTIONAL { ?s <http://www.w3.org/ns/adms#status> ?status. }
-        FILTER(!BOUND(?status) || ?status != <http://lblod.data.gift/concepts/9bd8d86d-bb10-4456-a84e-91e9507c374c>)`,
+        ?s a <http://data.vlaanderen.be/ns/subsidie#SubsidiemaatregelConsumptie> ;
+           <http://www.w3.org/ns/adms#status> ?status .
+      `
     }
   },
-  abb: {
-    "http://lblod.data.gift/vocabularies/subsidie/ApplicationForm": {},
-    "http://www.w3.org/ns/adms#Status": {},
-  },
-  internal: {
-    "http://lblod.data.gift/vocabularies/subsidie/ApplicationForm": {},
-    "http://www.w3.org/ns/adms#Status": {},
-  },
+  abb: {},
+  internal: {}
 };
-
-Object.keys(regularTypes).forEach((type) => {
-  const level = regularTypes[type];
-  if (level === "public") {
-    initialization.public[type] = {};
-    initialization.organization[type] = {};
-  } else if (level === "organization") {
-    initialization.organization[type] = {};
-  }
-});
