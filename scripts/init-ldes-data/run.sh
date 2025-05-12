5a9b9ba066c1da97bc21a1bcd624a17163e9f3b3
Achraf <achrafatauil@hotmail.com>
2025-05-12 15:51:18 +0200

    dirty commit

--- scripts/init-ldes-data/run.sh
+++ scripts/init-ldes-data/run.sh
@@ -3,4 +3,4 @@ mkdir -p /data/app/data/files
 npm i
 # we bypass mu auth to avoid producing delta messages that could be consumed by the ldes consumer
 export BASE_URL=$1
-MU_SPARQL_ENDPOINT=http://triplestore:8890/sparql node app.mjs
+MU_SPARQL_ENDPOINT=http://localhost:8890/sparql node app.mjs
