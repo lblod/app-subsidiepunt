34eabbdafbae39dc8d1aef3f50c293fed50963e7
felix <ruiz.felixruiz@gmail.com>
2025-05-13 10:01:59 +0200

    Remove scripts content, keep the skeleton folder

--- scripts/init-ldes-data/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-mkdir -p /data/app/data/files
-npm i
-# we bypass mu auth to avoid producing delta messages that could be consumed by the ldes consumer
-export BASE_URL=$1
-MU_SPARQL_ENDPOINT=http://localhost:8890/sparql node app.mjs
