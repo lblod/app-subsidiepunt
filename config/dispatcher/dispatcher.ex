1e7892ad39080c6c32bce90ace23f8a60bb7c36b
benjay10 <ben.behaeghe@redpencil.io>
2025-06-11 02:32:06 +0200

    Dispatcher: add accept types, comment unused rules

--- config/dispatcher/dispatcher.ex
+++ config/dispatcher/dispatcher.ex
@@ -17,100 +17,103 @@ defmodule Dispatcher do
   # Frontend resources
   #############################################################################
 
-  match "/organizations/*path" do
+  match "/organizations/*path", @json do
     forward conn, path, "http://cache/organizations/"
   end
 
-  match "/organization-classification-codes/*path" do
+  match "/organization-classification-codes/*path", @json do
     forward conn, path, "http://cache/organization-classification-codes/"
   end
 
-  match "/bestuurseenheden/*path" do
+  match "/bestuurseenheden/*path", @json do
     forward conn, path, "http://cache/bestuurseenheden/"
   end
 
-  match "/werkingsgebieden/*path" do
-    forward conn, path, "http://cache/werkingsgebieden/"
-  end
+  # match "/werkingsgebieden/*path" do
+  #   forward conn, path, "http://cache/werkingsgebieden/"
+  # end
 
-  match "/bestuurseenheid-classificatie-codes/*path" do
+  match "/bestuurseenheid-classificatie-codes/*path", @json do
     forward conn, path, "http://cache/bestuurseenheid-classificatie-codes/"
   end
 
-  match "/bestuursorganen/*path" do
-    forward conn, path, "http://cache/bestuursorganen/"
-  end
+  # match "/bestuursorganen/*path" do
+  #   forward conn, path, "http://cache/bestuursorganen/"
+  # end
 
-  match "/bestuursorgaan-classificatie-codes/*path" do
-    forward conn, path, "http://cache/bestuursorgaan-classificatie-codes/"
-  end
+  # match "/bestuursorgaan-classificatie-codes/*path" do
+  #   forward conn, path, "http://cache/bestuursorgaan-classificatie-codes/"
+  # end
 
   # TODO: remove?
   # match "/personen/*path" do
   #   forward conn, path, "http://cache/personen/"
   # end
 
-  match "/document-statuses/*path" do
-    forward conn, path, "http://cache/document-statuses/"
-  end
+  # match "/document-statuses/*path" do
+  #   forward conn, path, "http://cache/document-statuses/"
+  # end
 
-  match "/submission-document-statuses/*path" do
+  match "/submission-document-statuses/*path", @json do
     forward conn, path, "http://cache/submission-document-statuses/"
   end
 
-  match "/file-addresses/*path" do
-    forward conn, path, "http://resource/file-addresses/"
-  end
+  # match "/file-addresses/*path" do
+  #   forward conn, path, "http://resource/file-addresses/"
+  # end
 
-  match "/file-address-cache-statuses/*path" do
-    forward conn, path, "http://resource/file-address-cache-statuses/"
-  end
+  # match "/file-address-cache-statuses/*path" do
+  #   forward conn, path, "http://resource/file-address-cache-statuses/"
+  # end
 
-  match "/datasets/*path" do
-    forward conn, path, "http://cache/datasets/"
-  end
+  # match "/datasets/*path" do
+  #   forward conn, path, "http://cache/datasets/"
+  # end
 
-  match "/distributions/*path" do
-    forward conn, path, "http://cache/distributions/"
+  # match "/distributions/*path" do
+  #   forward conn, path, "http://cache/distributions/"
+  # end
+
+  # NOTE: resources
+  match "/remote-urls/*path", @json do
+    forward conn, path, "http://resource/remote-urls/"
   end
 
   #############################################################################
   # Frontend dashboard resources
   #############################################################################
 
-  match "/reports/*path" do
+  # NOTE: resources
+  match "/reports/*path", @json do
     forward conn, path, "http://resource/reports/"
   end
 
-  match "/remote-urls/*path" do
-    forward conn, path, "http://resource/remote-urls/"
-  end
+  # NOTE: resources
+  # get "/form-data/*path" do
+  #   forward conn, path, "http://resource/form-data/"
+  # end
 
-  get "/form-data/*path" do
-    forward conn, path, "http://resource/form-data/"
-  end
+  # get "/concept-schemes/*path" do
+  #   forward conn, path, "http://cache/concept-schemes/"
+  # end
 
-  get "/concept-schemes/*path" do
-    forward conn, path, "http://cache/concept-schemes/"
-  end
+  # get "/concepts/*path" do
+  #   forward conn, path, "http://cache/concepts/"
+  # end
 
-  get "/concepts/*path" do
-    forward conn, path, "http://cache/concepts/"
-  end
-
-  match "/jobs/*path" do
+  match "/jobs/*path", @json do
     forward conn, path, "http://cache/jobs/"
   end
 
-  match "/tasks/*path" do
+  match "/tasks/*path", @json do
     forward conn, path, "http://cache/tasks/"
   end
 
-  match "/data-containers/*path" do
+  match "/data-containers/*path", @json do
     forward conn, path, "http://cache/data-containers/"
   end
 
-  match "/job-errors/*path"  do
+  match "/job-errors/*path", @json do
     forward conn, path, "http://cache/job-errors/"
   end
 
@@ -122,11 +125,13 @@ defmodule Dispatcher do
     forward conn, [], "http://file/files/" <> id <> "/download"
   end
 
-  get "/files/*path" do
+  # NOTE: resources
+  get "/files/*path", @json do
     forward conn, path, "http://resource/files/"
   end
 
-  patch "/files/*path" do
+  # NOTE: resources
+  patch "/files/*path", @json do
     forward conn, path, "http://resource/files/"
   end
 
@@ -134,37 +139,39 @@ defmodule Dispatcher do
     forward conn, path, "http://file/files/"
   end
 
-  # TODO: find all usage of this endpoint and replace it with `POST /files`
-  # This is kept to maintain compatibility with code that uses the "old" endpoint.
-  post "/file-service/files/*path" do
-    forward conn, path, "http://file/files/"
-  end
-
   delete "/files/*path" do
     forward conn, path, "http://file/files/"
   end
+
+  # TODO: find all usage of this endpoint and replace it with `POST /files`
+  # This is kept to maintain compatibility with code that uses the "old" endpoint.
+  # UPDATE: nothing found in the frontend on 2025-06-11
+  # post "/file-service/files/*path" do
+  #   forward conn, path, "http://file/files/"
+  # end
 
   #############################################################################
   # Session management
   #############################################################################
 
-  match "/impersonations/*path" do
+  match "/impersonations/*path", @json do
     forward conn, path, "http://impersonation/impersonations/"
   end
 
-  match "/mock/sessions/*path" do
+  match "/mock/sessions/*path", @json do
     forward conn, path, "http://mocklogin/sessions/"
   end
 
-  match "/sessions/*path" do
+  match "/sessions/*path", @json do
     forward conn, path, "http://login/sessions/"
   end
 
-  match "/gebruikers/*path" do
+  match "/gebruikers/*path", @json do
     forward conn, path, "http://cache/gebruikers/"
   end
 
-  match "/accounts/*path" do
+  # NOTE: resources
+  match "/accounts/*path", @json do
     forward conn, path, "http://resource/accounts/"
   end
 
@@ -208,63 +215,63 @@ defmodule Dispatcher do
   # subsidy-applications: resources
   #############################################################################
 
-  match "/subsidy-measure-consumptions/*path" do
+  match "/subsidy-measure-consumptions/*path", @json do
     forward conn, path, "http://cache/subsidy-measure-consumptions/"
   end
 
-  match "/subsidy-measure-consumption-statuses/*path" do
+  match "/subsidy-measure-consumption-statuses/*path", @json do
     forward conn, path, "http://cache/subsidy-measure-consumption-statuses/"
   end
 
-  match "/subsidy-requests/*path" do
-    forward conn, path, "http://cache/subsidy-requests/"
-  end
+  # match "/subsidy-requests/*path" do
+  #   forward conn, path, "http://cache/subsidy-requests/"
+  # end
 
-  match "/monetary-amounts/*path" do
-    forward conn, path, "http://cache/monetary-amounts/"
-  end
+  # match "/monetary-amounts/*path" do
+  #   forward conn, path, "http://cache/monetary-amounts/"
+  # end
 
-  match "/subsidy-measure-offers/*path" do
+  match "/subsidy-measure-offers/*path", @json do
     forward conn, path, "http://cache/subsidy-measure-offers/"
   end
 
-  match "/subsidy-measure-offer-series/*path" do
+  match "/subsidy-measure-offer-series/*path", @json do
     forward conn, path, "http://cache/subsidy-measure-offer-series/"
   end
 
-  match "/subsidy-application-flows/*path" do
+  match "/subsidy-application-flows/*path", @json do
     forward conn, path, "http://cache/subsidy-application-flows/"
   end
 
-  match "/subsidy-application-flow-steps/*path" do
+  match "/subsidy-application-flow-steps/*path", @json do
     forward conn, path, "http://cache/subsidy-application-flow-steps/"
   end
 
-  match "/subsidy-procedural-steps/*path" do
+  match "/subsidy-procedural-steps/*path", @json do
     forward conn, path, "http://cache/subsidy-procedural-steps/"
   end
 
-  match "/periods-of-time/*path" do
+  match "/periods-of-time/*path", @json do
     forward conn, path, "http://cache/periods-of-time/"
   end
 
-  match "/criteria/*path" do
-    forward conn, path, "http://cache/criteria/"
-  end
+  # match "/criteria/*path" do
+  #   forward conn, path, "http://cache/criteria/"
+  # end
 
-  match "/requirement-groups/*path" do
-    forward conn, path, "http://cache/requirement-groups/"
-  end
+  # match "/requirement-groups/*path" do
+  #   forward conn, path, "http://cache/requirement-groups/"
+  # end
 
-  match "/criterion-requirements/*path" do
-    forward conn, path, "http://cache/criterion-requirements/"
-  end
+  # match "/criterion-requirements/*path" do
+  #   forward conn, path, "http://cache/criterion-requirements/"
+  # end
 
-  match "/participations/*path" do
+  match "/participations/*path", @json do
     forward conn, path, "http://cache/participations/"
   end
 
-  match "/subsidy-application-forms/*path" do
+  match "/subsidy-application-forms/*path", @json do
     forward conn, path, "http://cache/subsidy-application-forms/"
   end
 
@@ -280,23 +287,24 @@ defmodule Dispatcher do
     forward conn, path, "http://case-number-generator/"
   end
 
-  get "/management-active-form-file/*path" do
-    forward conn, path, "http://subsidy-applications-management/active-form-file/"
-  end
+  # NOTE: does not exist in the service
+  # get "/management-active-form-file/*path", @json do
+  #   forward conn, path, "http://subsidy-applications-management/active-form-file/"
+  # end
 
-  get "/management-application-forms/*path" do
+  get "/management-application-forms/*path", @json do
     forward conn, path, "http://subsidy-applications-management/semantic-forms/"
   end
 
-  put "/management-application-forms/*path" do
+  put "/management-application-forms/*path", @json do
     forward conn, path, "http://subsidy-applications-management/semantic-forms/"
   end
 
-  delete "/management-application-forms/*path" do
+  delete "/management-application-forms/*path", @json do
     forward conn, path, "http://subsidy-applications-management/semantic-forms/"
   end
 
-  post "/management-application-forms/:id/submit" do
+  post "/management-application-forms/:id/submit", @json do
     forward conn, [], "http://subsidy-applications-management/semantic-forms/" <> id <> "/submit"
   end
 
@@ -308,7 +316,7 @@ defmodule Dispatcher do
   # Others
   #############################################################################
 
-  match "/*_" do
+  match "/*_", @any do
     send_resp( conn, 404, "Route not found.  See config/dispatcher.ex" )
   end
 
