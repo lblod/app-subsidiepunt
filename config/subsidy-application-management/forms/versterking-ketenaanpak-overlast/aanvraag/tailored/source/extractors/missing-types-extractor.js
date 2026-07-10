96c4993ff025ecea5ec022d2a2f912f689ed82dd
Achraf <achrafatauil@hotmail.com>
2026-07-10 13:30:09 +0200

    add extractor for new form

--- config/subsidy-application-management/forms/versterking-ketenaanpak-overlast/aanvraag/tailored/source/extractors/missing-types-extractor.js
+++ config/subsidy-application-management/forms/versterking-ketenaanpak-overlast/aanvraag/tailored/source/extractors/missing-types-extractor.js
@@ -1,33 +1,38 @@
 const URI_BASE = 'http://data.lblod.info/form-data/nodes/';
 
 module.exports = {
-  name: 'ruimte-vrouwen-en-meisjes/aanvraag/missing-types-extractor',
+  name: 'versterking-ketenaanpak-overlast/aanvraag/missing-types-extractor',
   execute: async (store, graphs, lib, source) => {
     const {$rdf, mu, sudo} = lib;
 
     const RDF_TYPE = new $rdf.NamedNode('http://www.w3.org/1999/02/22-rdf-syntax-ns#type');
+    const SCHEMA = new $rdf.Namespace('http://schema.org/');
     const SUBSIDIE = new $rdf.Namespace('http://lblod.data.gift/vocabularies/subsidie/');
+    const EXT = new $rdf.Namespace('http://mu.semte.ch/vocabularies/ext/');
 
-    const OVERZICHT_ERKENNINGEN = new $rdf.Namespace('http://lblod.data.gift/vocabularies/subsidie/overzichtErkenningen/');
-    const BEGROTING_DOCUMENT = new $rdf.Namespace('http://lblod.data.gift/vocabularies/subsidie/begrotingDocument/');
     const BIJLAGE_DOCUMENT = new $rdf.Namespace('http://lblod.data.gift/vocabularies/subsidie/bijlageDocument/');
-    const PROJECTPLAN_DOCUMENT = new $rdf.Namespace('http://lblod.data.gift/vocabularies/subsidie/projectPlanDocument/');
+    const FINANCING_PARTNER = new $rdf.Namespace('http://lblod.data.gift/vocabularies/subsidie/financingPartner/');
+    const SUBSIDIE_BEDRAG_LISTING_UNIT = new $rdf.Namespace('http://lblod.data.gift/vocabularies/subsidie/subsidieBedragListingUnit/');
 
-    const overzichtErkenningen = new $rdf.NamedNode(URI_BASE + mu.uuid());
-    const begrotingDocument = new $rdf.NamedNode(URI_BASE + mu.uuid());
+    const contactPoint = new $rdf.NamedNode(URI_BASE + mu.uuid());
+    const politicalResponsibleContactPoint = new $rdf.NamedNode(URI_BASE + mu.uuid());
+    const subsidieBedragListingUnit = new $rdf.NamedNode(URI_BASE + mu.uuid());
+    const financingPartner = new $rdf.NamedNode(URI_BASE + mu.uuid());
     const bijlageDocument = new $rdf.NamedNode(URI_BASE + mu.uuid());
-    const projectPlanDocument = new $rdf.NamedNode(URI_BASE + mu.uuid());
 
-    store.add($rdf.sym(source.uri), SUBSIDIE('overzichtErkenningen'), $rdf.sym(overzichtErkenningen), graphs.additions);
-    store.add($rdf.sym(overzichtErkenningen), RDF_TYPE, OVERZICHT_ERKENNINGEN('FormData'), graphs.additions);
+    store.add($rdf.sym(source.uri), SCHEMA('contactPoint'), $rdf.sym(contactPoint), graphs.additions);
+    store.add($rdf.sym(contactPoint), RDF_TYPE, SCHEMA('ContactPoint'), graphs.additions);
 
-    store.add($rdf.sym(source.uri), SUBSIDIE('begrotingDocument'), $rdf.sym(begrotingDocument), graphs.additions);
-    store.add($rdf.sym(begrotingDocument), RDF_TYPE, BEGROTING_DOCUMENT('FormData'), graphs.additions);
+    store.add($rdf.sym(source.uri), EXT('politicalResponsibleContactPoint'), $rdf.sym(politicalResponsibleContactPoint), graphs.additions);
+    store.add($rdf.sym(politicalResponsibleContactPoint), RDF_TYPE, EXT('PoliticalResponsibleContactPoint'), graphs.additions);
+
+    store.add($rdf.sym(source.uri), SUBSIDIE('subsidieBedragListingUnit'), $rdf.sym(subsidieBedragListingUnit), graphs.additions);
+    store.add($rdf.sym(subsidieBedragListingUnit), RDF_TYPE, SUBSIDIE_BEDRAG_LISTING_UNIT('FormData'), graphs.additions);
+
+    store.add($rdf.sym(source.uri), EXT('financingPartner'), $rdf.sym(financingPartner), graphs.additions);
+    store.add($rdf.sym(financingPartner), RDF_TYPE, FINANCING_PARTNER('FormData'), graphs.additions);
 
     store.add($rdf.sym(source.uri), SUBSIDIE('bijlageDocument'), $rdf.sym(bijlageDocument), graphs.additions);
     store.add($rdf.sym(bijlageDocument), RDF_TYPE, BIJLAGE_DOCUMENT('FormData'), graphs.additions);
-
-    store.add($rdf.sym(source.uri), SUBSIDIE('projectPlan'), $rdf.sym(projectPlanDocument), graphs.additions);
-    store.add($rdf.sym(projectPlanDocument), RDF_TYPE, PROJECTPLAN_DOCUMENT('FormData'), graphs.additions);
   }
-} 
+}
